9b0aa737c321da34ea4e7646b16bfbdf71076699
Gayatri <gayatri29mehta@gmail.com>
2023-07-09 17:23:18 +0530

    polybot updt

--- polybot/app.py
+++ polybot/app.py
@@ -2,14 +2,19 @@ import telebot
 from loguru import logger
 import os
 import requests
+from collections import Counter
+import ffmpeg
+
 
 YOLO_URL = 'http://localhost:8081'
 
 
 class Bot:
+
     def __init__(self, token):
         self.bot = telebot.TeleBot(token, threaded=False)
         self.bot.set_update_listener(self._bot_internal_handler)
+
         self.current_msg = None
 
     def _bot_internal_handler(self, messages):
@@ -22,16 +27,20 @@ class Bot:
         """Start polling msgs from users, this function never returns"""
         logger.info(f'{self.__class__.__name__} is up and listening to new messages....')
         logger.info(f'Telegram Bot information\n\n{self.bot.get_me()}')
+
         self.bot.infinity_polling()
 
-    def send_text(self, chat_id, text):
-        self.bot.send_message(chat_id, text)
+    def send_text(self, text):
+        self.bot.send_message(self.current_msg.chat.id, text)
 
-    def send_text_with_quote(self, chat_id, text, message_id):
-        self.bot.send_message(chat_id, text, reply_to_message_id=message_id)
+    def send_text_with_quote(self, text, message_id):
+        self.bot.send_message(self.current_msg.chat.id, text, reply_to_message_id=message_id)
 
     def is_current_msg_photo(self):
         return self.current_msg.content_type == 'photo'
+
+    def is_current_msg_video(self):
+        return self.current_msg.content_type == 'video'
 
     def download_user_photo(self, quality=2):
         """
@@ -40,9 +49,32 @@ class Bot:
         :return:
         """
         if not self.is_current_msg_photo():
-            raise RuntimeError(f'Message content of type \'photo\' expected, but got {self.current_msg.content_type}')
+            raise RuntimeError(
+                f'Message content of type \'photo\' expected, but got {self.current_msg.content_type}')
 
         file_info = self.bot.get_file(self.current_msg.photo[quality].file_id)
+        data = self.bot.download_file(file_info.file_path)
+        folder_name = file_info.file_path.split('/')[0]
+
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+
+        with open(file_info.file_path, 'wb') as photo:
+            photo.write(data)
+
+        return file_info.file_path
+
+    def download_user_video(self, quality=2):
+        """
+        Downloads the photos that sent to the Bot to `photos` directory (should be existed)
+        :param quality: integer representing the file quality. Allowed values are [0, 1, 2]
+        :return:
+        """
+        if not self.is_current_msg_video():
+            raise RuntimeError(
+                f'Message content of type \'video\' expected, but got {self.current_msg.content_type}')
+
+        file_info = self.bot.get_file(self.current_msg.video.file_id)
         data = self.bot.download_file(file_info.file_path)
         folder_name = file_info.file_path.split('/')[0]
 
@@ -57,7 +89,7 @@ class Bot:
     def handle_message(self, message):
         """Bot Main message handler"""
         logger.info(f'Incoming message: {message}')
-        self.send_text(message.chat.id, f'Your original message: {message.text}')
+        self.send_text(f'Your original message: {message.text}')
 
 
 class QuoteBot(Bot):
@@ -65,42 +97,49 @@ class QuoteBot(Bot):
         logger.info(f'Incoming message: {message}')
 
         if message.text != 'Please don\'t quote me':
-            self.send_text_with_quote(message.chat.id, message.text, message_id=message.message_id)
+            self.send_text_with_quote(message.text, message_id=message.message_id)
 
 
 class ObjectDetectionBot(Bot):
+
     def handle_message(self, message):
         logger.info(f'Incoming message: {message}')
 
-        if message.content_type == 'photo':
-            file_path = self.download_user_photo()
-            detected_objects = self.detect_objects(file_path)
+        if self.is_current_msg_video():
+            video_path = self.download_user_video()
 
-            if detected_objects:
-                object_names = ", ".join(detected_objects)
-                response_text = f"Detected objects: {object_names}"
-                self.send_text_with_quote(message.chat.id, response_text, message_id=message.message_id)
+            if os.path.exists('frames'):
+                os.rmdir('frames')
+
+            os.mkdir('frames')
+
+            output_file = 'frames/output_%04d.jpg'
+            framerate = '1'
+            ffmpeg.input(video_path).output(output_file, r=framerate).run()
+
+        elif self.is_current_msg_photo():
+            photo_path = self.download_user_photo()
+
+            res = requests.post(f'{YOLO_URL}/predict', files={
+                'file': (photo_path, open(photo_path, 'rb'), 'image/png')})
+
+            if res.status_code == 200:
+                detections = res.json()
+                logger.info(f'response from detect service with {detections}')
+
+                """calc summary"""
+                element_counts = Counter([l['class'] for l in detections])
+                summary = 'Objects Detected:\n'
+                for element, count in element_counts.items():
+                    summary += f"{element}: {count}\n"
+
+                self.send_text(summary)
+
             else:
-                self.send_text_with_quote(message.chat.id, "No objects detected in the image.", message_id=message.message_id)
+                self.send_text('Failed to perform object detection. Please try again later.')
+
         else:
-            self.send_text_with_quote(message.chat.id, message.text, message_id=message.message_id)
-
-    def detect_objects(self, image_file_path):
-        # Implement the logic to send the image to the yolo5 service and get the detected objects
-        # Use the requests library to communicate with the yolo5 service
-        # Parse the response and extract the detected objects
-
-        # For example:
-        yolo5_url = f"{YOLO_URL}/detect"
-        files = {"image": open(image_file_path, "rb")}
-        response = requests.post(yolo5_url, files=files)
-
-        if response.status_code == 200:
-            detected_objects = response.json()
-            return detected_objects
-        else:
-            # Handle error cases if necessary
-            return []
+            self.send_text('please send a photo for object detection.')
 
 
 if __name__ == '__main__':
